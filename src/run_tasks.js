71d7092b45669d3026d5826634a921e5d52ab0d0
Griffin Solot-Kehl <griffin@transposit.com>
2019-08-13 00:50:08 +0000

    Commit from Transposit IDE

--- src/run_tasks.js
+++ src/run_tasks.js
@@ -1,11 +1,11 @@
 (params) => {
-  let newSheet = api.run("this.create_sheet", {name: `Reviews for ${env.get("url")}`});
+  //let newSheet = api.run("this.create_sheet", {name: `Reviews for ${env.get("url")}`});
   //api.run("this.scrape_reviews", { url: env.get("url") });
   let reviews = api.run("this.get_reviews");
   let values = [];
   for (let i = 0; i < reviews.length; i++) {
       values.push([reviews[i].rating, reviews[i].title, reviews[i].content, reviews[i].author, reviews[i].date]);
   }
-  //api.run('this.sheets_copy', {sheetId: newSheet[0].spreadsheetUrl, values: values});
-  return newSheet[0].spreadsheetUrl;
+  api.run('this.sheets_copy', {sheetId: '1WRfl5afcq1P7Obp1bBa5nVm0Vy0aFZ3OmLXDqVb73f4', values: values});
+  return 0;//newSheet[0].spreadsheetUrl;
 }
