5c4068e10660a4d5c2ef346bd6136ef882093350
Griffin Solot-Kehl <griffin@transposit.com>
2019-08-27 20:44:32 +0000

    Clean up code

--- src/run_tasks.js
+++ src/run_tasks.js
@@ -1,9 +1,10 @@
 (params) => {
   // Create a new sheet to store the data
-  let newSheet = api.run("this.create_sheet", {name: `Reviews for ${env.get("url")}`});
+  let newSheet = api.query('SELECT spreadsheetId, spreadsheetUrl FROM google_sheets.create_sheet WHERE $body.properties.title=@name',
+  	{name: `Reviews for ${env.get("url")}`});
   // Scrape the reviews and get the data
   api.run("this.scrape_reviews", { url: env.get("url") });
-  let reviews = api.run("this.get_reviews");
+  let reviews = api.query('SELECT rating, title, content, author, date FROM apify_v2.get_items_in_task_last_run_dataset WHERE actorTaskId="wbW84AN7b8AtEFtEZ" AND status="SUCCEEDED"');
   // Populate the headers and data into a parsable array
   let values = [["Rating", "Title", "Content", "Author", "Date"]];
   for (let i = 0; i < reviews.length; i++) {
